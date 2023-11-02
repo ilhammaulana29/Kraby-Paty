83d787e0e1d3d18d8d3c5b217a537a01e23546e2
PUJA AYU TRISNANDA <pujaayut@gmail.com>
2023-11-02 09:46:35 +0700

    saya sudah selesai mengerjakan resep kraby paty terima kasih

--- resep.php
+++ resep.php
@@ -1,4 +1,11 @@
 <?php
 echo "Resep Kraby Paty";
+echo "-----------------";
 echo "1.Bawang";
-echo "Ketimun";
+echo "2.Ketimun";
+echo "3.Selada";
+echo "4.Tomat";
+echo "5.Daging";
+echo "6.Keju";
+echo "7.Mayonaise";
+echo "8.Roti";
